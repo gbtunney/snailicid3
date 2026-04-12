8bd2d638ee150cf8f6af6783da086dee7d066c29
Gillian Tunney <gbtunney@mac.com>
2026-04-12 08:40:51 +0000

    feat: publish-harden config build-config and types

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,9 +1,41 @@
-// @snailicid3/config
-// TODO: implement
+export { EsLint, config, flatEslintConfig } from './eslint/index.js'
+export type { EslintConfig, TsConfig } from './eslint/index.js'
 
-export type HelloWorld = string | number
+export {
+    Prettier,
+    SHARED_FORMATTING_RULES,
+    getScaledWidth,
+    prettierConfiguration,
+} from './prettier/index.js'
+export type { PrettierConfig, PrettierOptions, PrettierOverrides } from './prettier/index.js'
 
-/** Returns the input value unchanged. */
-export function echoHelloWorld(value: HelloWorld): HelloWorld {
-    return value
-}
+export { COMMIT_TYPES, commitlint, configuration } from './commitlint/index.js'
+export type { CommitlintUserConfig } from './commitlint/index.js'
+
+export { markdownLintConfigJson, markdownlint } from './markdownlint/index.js'
+export type {
+    MarkdownlintAPI,
+    MarkdownlintCli2ConfigurationSchema,
+    MarkdownlintConfiguration,
+    MarkdownlintConfigurationSchema,
+    MarkdownlintOpts,
+    MarkdownlintProcessedResult,
+    MarkdownlintRuleConfiguration,
+} from './markdownlint/index.js'
+
+export {
+    JSLIKE_FILE_EXTENSIONS,
+    JS_FILE_EXTENSIONS,
+    TS_FILE_EXTENSIONS,
+    getFileExtensionList,
+    getFilePath,
+    importJSON,
+    isPlainObject,
+    safeDeserializeJSON,
+} from './utilities.js'
+export type {
+    AllowedExtensions,
+    JSFileExtensions,
+    JSLikeFileExtensions,
+    TSFileExtensions,
+} from './utilities.js'
