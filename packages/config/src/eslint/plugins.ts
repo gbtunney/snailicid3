dbfd69a4f82ec2859e8120d62a9693c287190a8e
Gillian Tunney <gbtunney@mac.com>
2026-04-18 05:46:25 +0000

    feat(adde unicorn and file check): adde unicorn and file check

--- packages/config/src/eslint/plugins.ts
+++ packages/config/src/eslint/plugins.ts
@@ -1,14 +1,31 @@
-import { type Config, defineConfig } from '@eslint/config-helpers'
+/** PLUGIN REGISTRY */
+import {
+    type Config,
+    defineConfig,
+    Plugin as EslintPlugin,
+} from '@eslint/config-helpers'
 import eslintCommentsPlugin from '@eslint-community/eslint-plugin-eslint-comments'
 import vitestPlugin from '@vitest/eslint-plugin'
 import checkFilePlugin from 'eslint-plugin-check-file'
 import importPlugin from 'eslint-plugin-import'
 import jsdocPlugin from 'eslint-plugin-jsdoc'
+import _reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import sortPlugin from 'eslint-plugin-sort'
+import unicornPlugin from 'eslint-plugin-unicorn'
 import unusedImports from 'eslint-plugin-unused-imports'
 //import type { Config } from 'typescript-eslint'
 import tsEslint from 'typescript-eslint'
+
+/**
+ * This canot use a function to create the plugin config because of the way eslint expects plugins to be defined. so
+ * annoying
+ */
+
+const reactHooks: EslintPlugin = {
+    meta: _reactHooks.meta,
+    rules: _reactHooks.rules,
+}
 
 export const pluginsConfig = (): Array<Config> =>
     defineConfig([
@@ -18,12 +35,13 @@ export const pluginsConfig = (): Array<Config> =>
                 ['@typescript-eslint']: tsEslint.plugin,
                 ['check-file']: checkFilePlugin,
                 ['eslint-comments']: eslintCommentsPlugin,
-                //['filenames-simple']: simpleFilenamesPlugin,
+
                 ['import']: importPlugin,
                 ['jsdoc']: jsdocPlugin,
-                //   ['react-hooks']: reactHooks,
+                ['react-hooks']: reactHooks,
                 ['react-refresh']: reactRefresh,
                 ['sort']: sortPlugin,
+                ['unicorn']: unicornPlugin,
                 ['unused-imports']: unusedImports,
                 ['vitest']: vitestPlugin,
             },
