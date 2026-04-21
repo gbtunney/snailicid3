d34dfc3f63f90de03a614a7a02e6834f46fac736
Gillian Tunney <gbtunney@mac.com>
2026-04-21 02:07:54 +0000

    fix(root): fixed busted nx config and commitlint deps

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,23 +3,22 @@
  * @author Gillian Tunney
  * @see [commitlint - Lint commit messages](https://commitlint.js.org/#/)
  */
-import { commitlint,CommitlintUserConfig } from '@snailicide/build-config'
+import { commitlint, CommitlintUserConfig } from '@snailicid3/config'
 
-const Configuration: CommitlintUserConfig = commitlint.configuration([
-"root",
-"@snailicid3/config",
-"@snailicid3/build-config",
-"@snailicid3/types",
-"@snailicid3/utils",
-"@snailicid3/cli-app",
-"@snailicid3/color",
-"notes",
-"@snailicid3/example-package",
-"@snailicid3/logger",
-"@snailicid3/node-utils",
-"@snailicid3/scaffold",
-"@snailicid3/workspace-tools",
-"@snailicid3/playground"
+const configuration: CommitlintUserConfig = commitlint.configuration([
+    'root',
+    '@snailicid3/config',
+    '@snailicid3/build-config',
+    '@snailicid3/types',
+    '@snailicid3/utils',
+    '@snailicid3/cli-app',
+    '@snailicid3/color',
+    'notes',
+    '@snailicid3/example-package',
+    '@snailicid3/logger',
+    '@snailicid3/node-utils',
+    '@snailicid3/scaffold',
+    '@snailicid3/workspace-tools',
+    '@snailicid3/playground',
 ])
-
-export default Configuration
+export default configuration
