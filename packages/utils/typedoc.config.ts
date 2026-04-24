3d69bfdc46563cde81b97db6cea8c00a7d4f2576
Gillian Tunney <gbtunney@mac.com>
2026-04-24 04:28:59 +0000

    refactor(root,@snailicid3/build-config,@snailicid3/utils,@snailicid3/cli-app): typedoc,renamed

---
+++ packages/utils/typedoc.config.ts
@@ -0,0 +1,29 @@
+import { merge, typedoc } from '@snailicid3/build-config'
+import { TypeDocOptions } from 'typedoc'
+import url from 'node:url'
+import path from 'path'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+//import { Options as OptionsVitepress }from 'typedoc-vitepress-theme'
+
+/** @todo: needs to be updated to vitepress */
+const custom: Partial<TypeDocOptions> = {
+    //  categorizeByGroup: true,
+    entryPoints: [path.resolve(`${__dirname}/src/index.ts`)],
+    /* excludeInternal: true,
+    navigation: {
+        includeCategories: true,
+        includeFolders: false,
+        includeGroups: true,
+    },*/
+    //plugin: ['typedoc-plugin-remove-references'],
+}
+
+const defaultConfig = typedoc.materialTheme(__dirname)
+
+const _typeDocConfig = merge(
+    defaultConfig !== undefined ? defaultConfig : {},
+    custom,
+)
+
+const typeDocConfig: ReturnType<typeof typedoc.materialTheme> = _typeDocConfig
+export default typeDocConfig
