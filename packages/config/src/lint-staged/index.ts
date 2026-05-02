9b8a359bace53a743f322c1fa5312608ee2c75a9
Gillian Tunney <gbtunney@mac.com>
2026-05-02 07:38:04 +0000

    fix(config, root): lint staged working again

--- packages/config/src/lint-staged/index.ts
+++ packages/config/src/lint-staged/index.ts
@@ -1,43 +1,52 @@
 import { type Configuration } from 'lint-staged'
-import {JSLIKE_FILE_EXTENSIONS, PRETTIER_FILE_EXTENSIONS}from './../shared.js'
+import {
+    JSLIKE_FILE_EXTENSIONS,
+    PRETTIER_FILE_EXTENSIONS,
+} from './../shared.js'
+
+const extensionsToGlob = (extensions: ReadonlyArray<string>): string =>
+    `*.{${extensions.join(',')}}`
 
 export const quoteArg = (p: string): string => `"${p.replaceAll('"', '\\"')}"`
 export const toFileArgs = (staged: ReadonlyArray<string> | string): string =>
     (Array.isArray(staged) ? staged : [staged]).map(quoteArg).join(' ')
 
-export const lintStagedConfig  = (): Configuration=> {
-    
-  const config :Configuration=   {
-    '.gitignore': 'pnpm exec prettier --write .gitignore',
+export const lintStagedConfig = (): Configuration => {
+    const config: Configuration = {
+        '.gitignore': 'pnpm exec prettier --write .gitignore',
 
-    '.husky/**/*': (staged: ReadonlyArray<string>) => {
-        const files = toFileArgs(staged)
-        return `pnpm exec prettier --write ${files}`
-    },
+        '.husky/**/*': (staged: ReadonlyArray<string>) => {
+            const files = toFileArgs(staged)
+            return `pnpm exec prettier --write ${files}`
+        },
 
-    [`*.${JSLIKE_FILE_EXTENSIONS}`]: (staged: ReadonlyArray<string>) => {
-        const files = toFileArgs(staged)
-        return [
-            `pnpm exec prettier --write ${files}`,
-            `pnpm exec eslint --fix ${files}`,
-        ]
-    },
+        [`*.md`]: (staged: ReadonlyArray<string>) => {
+            const files = toFileArgs(staged)
+            //  const ignores = toIgnoreArgs(mdIgnores)
+            return [
+                `pnpm exec prettier --write ${files}`,
+                `pnpm exec markdownlint-cli2 --fix ${files}  || true`,
+            ]
+        },
 
-    [`*.${PRETTIER_FILE_EXTENSIONS}`]: (staged: ReadonlyArray<string>) => {
-        const files = toFileArgs(staged)
-        return `pnpm exec prettier --write ${files}`
-    },
+        [extensionsToGlob(JSLIKE_FILE_EXTENSIONS)]: (
+            staged: ReadonlyArray<string>,
+        ) => {
+            const files = toFileArgs(staged)
+            return [
+                `pnpm exec prettier --write ${files}`,
+                `pnpm exec eslint --fix ${files}`,
+            ]
+        },
 
-    [`*.md`]: (staged: ReadonlyArray<string>) => {
-        const files = toFileArgs(staged)
-        //  const ignores = toIgnoreArgs(mdIgnores)
-        return [
-            `pnpm exec prettier --write ${files}`,
-            `pnpm exec markdownlint-cli2 --fix ${files}  || true`,
-        ]
-    },
-}
-return config   
+        [extensionsToGlob(PRETTIER_FILE_EXTENSIONS)]: (
+            staged: ReadonlyArray<string>,
+        ) => {
+            const files = toFileArgs(staged)
+            return `pnpm exec prettier --write ${files}`
+        },
+    }
+    return config
 }
 export const lintstaged = {
     configuration: lintStagedConfig,
