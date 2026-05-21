8f0557a2ac93da11031fe46b22397f72e079da8b
Gillian Tunney <gbtunney@mac.com>
2026-05-21 09:31:45 -0400

    fix(config,root): fixes #95

--- packages/config/src/build-exporter.ts
+++ packages/config/src/build-exporter.ts
@@ -1,9 +1,25 @@
 import type { JsonObject } from 'type-fest'
+import { apiExtractor } from './api-extractor/index.js'
 import { markdownlint } from './markdownlint/index.js'
 import { getPrettierPluginsList } from './prettier/plugins.js'
 import { exportJSONFile, isPlainObject } from './utilities/json.js'
 import { Prettier } from './index.js'
 /* TODO: outputs a json dump of markdownlint and prettier. this is TEMPORARY till the configs are correct. ideally this should be gotten rid of */
+
+const _apiExtractorConfig = apiExtractor.config()
+const API_EXTRACTOR_CONFIG = isPlainObject<JsonObject>(_apiExtractorConfig)
+    ? _apiExtractorConfig
+    : {}
+
+// Debug: log what will be written
+console.log(
+    '[build-exporter] API_EXTRACTOR_CONFIG keys:',
+    Object.keys(API_EXTRACTOR_CONFIG),
+)
+console.log(
+    '[build-exporter] API_EXTRACTOR_CONFIG preview:',
+    JSON.stringify(API_EXTRACTOR_CONFIG, null, 2).slice(0, 400),
+)
 
 const MARKDOWN_LINT_CONFIG = isPlainObject<JsonObject>(markdownlint.config())
     ? markdownlint.config()
@@ -27,6 +43,11 @@ const JSON_EXPORTS = [
     {
         data: MARKDOWN_LINT_CONFIG,
         filename: 'dist/.markdownlint.json',
+    },
+
+    {
+        data: API_EXTRACTOR_CONFIG,
+        filename: 'dist/.api-extractor-base.json',
     },
 ]
 
