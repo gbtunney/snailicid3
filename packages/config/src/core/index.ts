814482d2758fe5ce4c5a6ce40570aac71d7303d5
Gillian Tunney <gbtunney@mac.com>
2026-06-23 18:36:40 -0400

    feat(config): export config builder helper

--- packages/config/src/core/index.ts
+++ packages/config/src/core/index.ts
@@ -2,11 +2,15 @@ export type {
     AnyDefineConfig,
     BaseConfigFunctionOptions,
     ConfigBuilder,
+    ConfigBuilderImplementation,
     ConfigCwd,
     ConfigFunctionOptions,
     ConfigTool,
     ConfigToolApi,
     IdentityDefineConfig,
+    ResolvedBaseConfigFunctionOptions,
+    ResolvedConfigCwd,
+    ResolvedConfigFunctionOptions,
 } from './define-config.js'
 
-export { defineConfig, defineConfigTool } from './define-config.js'
+export { defineConfig, defineConfigBuilder, defineConfigTool } from './define-config.js'
