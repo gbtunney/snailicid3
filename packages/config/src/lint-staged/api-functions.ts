f379fb44871f336492df2fb68033a7bc6372d699
Gillian Tunney <gbtunney@mac.com>
2026-06-23 18:37:13 -0400

    feat(config): require lint-staged cwd

--- packages/config/src/lint-staged/api-functions.ts
+++ packages/config/src/lint-staged/api-functions.ts
@@ -1,7 +1,11 @@
 import { type Configuration } from 'lint-staged'
 
 import { getLintStagedDefaultConfig } from './base.js'
-import { type ConfigFunctionOptions, defineConfig } from '../core/index.js'
+import {
+    type ConfigFunctionOptions,
+    defineConfig,
+    defineConfigBuilder,
+} from '../core/index.js'
 
 export type LintStagedConfig = Configuration
 
@@ -14,13 +18,11 @@ export const defineLintStagedConfig = <const TConfig extends LintStagedConfig>(
     config: TConfig,
 ): TConfig => defineConfig(config)
 
-export const buildFunctionLintStaged = (
-    /** Merged on top of the default lint-staged config. */
-
-    options: LintStagedConfigFunctionOptions = {},
-): LintStagedConfig => {
-    const { cwd: _cwd, overrides = {} } = options
+export const buildFunctionLintStaged = defineConfigBuilder<
+    LintStagedConfig,
+    LintStagedConfigFunctionOptions
+>(({ overrides = {} }) => {
     const config: LintStagedConfig = getLintStagedDefaultConfig()
     const finalConfig = Object.assign({}, config, overrides)
     return defineLintStagedConfig(finalConfig)
-}
+})
