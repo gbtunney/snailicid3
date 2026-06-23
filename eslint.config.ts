3c564258d8a8c45355a334d23e29e8cfdf69a334
Gillian Tunney <gbtunney@mac.com>
2026-06-23 19:45:16 -0400

    fix(root): pass string cwd to eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,7 @@
 import { EsLint } from '@snailicid3/config'
+import { fileURLToPath } from 'node:url'
 
-const CONFIG = EsLint.config({ cwd: import.meta })
+const cwd = fileURLToPath(new URL('.', import.meta.url))
+const CONFIG = EsLint.config({ cwd })
 
 export default EsLint.defineConfig(CONFIG)
